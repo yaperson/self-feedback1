4c69e3b3bdf50846a1d39ac0d282ae0e0ee26a15
Yanis <yanis@netkoders.com>
2021-10-07 10:56:24 +0200

    YLG_UI_FirstBase

---
+++ src/Classes/Manager/UserManager.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Classes\Manager;
+
+use PDO;
+use App\Classes\Entity\User;
+
+class UserManager
+{
+    private $_db;
+
+    public function __construct(PDO $db)
+    {
+        $this->setDb($db);
+    }
+
+    public function setDb($db): UserManager
+    {
+        $this->_db = $db;
+        return $this;
+    }
+
+    public function addUser($email, $password)
+    {
+        // Netoyge des donnés envoyées
+        // $email = strip_tags($_POST['email']);
+        // $password = strip_tags($_POST['password']);
+
+        $stmt = $this->_db->prepare("INSERT INTO users (email, `password`) VALUE (?, ?);");
+        $stmt->bindParam(1, $email);
+        $stmt->bindParam(2, $password);
+
+        // Appel de la procédure stockée
+        $stmt->execute();
+    }
+
+    public function delete(User $user) //:bool
+
+    {
+        // TODO
+    }
+
+    public function update(User $user) //:bool
+
+    {
+        // TODO
+    }
+
+    public function connectUser($email, $password)
+    {
+        session_start();
+        $_SESSION["connecter"] = FALSE;
+        $request = $this->_db->query('SELECT id, email, `password` FROM users');
+    }
+
+    public function getList(): array
+    {
+        $userList = array();
+
+        $request = $this->_db->query('SELECT id, email FROM users;');
+        while ($ligne = $request->fetch(PDO::FETCH_ASSOC)) {
+            $user = new User($ligne);
+            $userList[] = $user;
+        }
+        return $userList;
+    }
+}
+
+
+?>
---
+++ src/connect.php
@@ -0,0 +1,57 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use App\Classes\Manager\UserManager;
+
+$logger = new Logger('main');
+
+$logger->pushHandler(new StreamHandler(__DIR__.'/../log/app.log', Logger::DEBUG));  // création anonyme
+
+$logger->info('Start...');
+
+$loader = new FilesystemLoader('../templates');
+
+$twig = new Environment($loader, ['cache' => '../cache']);
+
+$error = '';
+
+require_once("conf.php");
+
+try {
+    if (isset($_POST['email'])&&(isset($_POST['password']))){
+        $db = new PDO($dsn, $usr, $pwd);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $newuser = new UserManager($db);
+        $password = $_POST['password'];
+        // $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $newuser->connectUser($_POST['email'], $password);
+
+        while ($ligne = $request->fetch(PDO::FETCH_ASSOC)){
+            if ($email == $ligne['email']){
+               $hash = $ligne['password'];
+               if (password_verify($password, $hash)) {
+                   echo 'Le mot de passe est valide !';
+                   $_SESSION['connecter'] = TRUE;
+                   header('Location: userList.php');
+                } else {
+                   session_destroy();
+                   echo '<div class="error">Le mot de passe est invalide.</div>';
+                }
+            }
+        }
+    }
+     
+
+     
+} catch(PDOException $e) {
+    print('erreur de connection : ' . $e->getMessage());
+}     
+echo $twig->render('connect.html.twig', [
+    'title' => 'Connectez vous !!!!!!!!',
+    'error' => $error,
+    ]
+);    
