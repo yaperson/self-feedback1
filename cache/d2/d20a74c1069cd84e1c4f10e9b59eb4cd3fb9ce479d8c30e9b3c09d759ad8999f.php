c3bf4a89993e73f8f95c26611f3b68ef823f4f98
Luidjy Aubel <luidjy.aubel@gmail.com>
2021-11-17 20:12:00 +0100

    LAB_ClassAtoForm

---
+++ cache/d2/d20a74c1069cd84e1c4f10e9b59eb4cd3fb9ce479d8c30e9b3c09d759ad8999f.php
@@ -0,0 +1,102 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* connect.html.twig */
+class __TwigTemplate_afd4cc8ded317f6876968e60a59167290445f51bce11efbb8da732e376d06d14 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <link rel=\"stylesheet\" href=\"/style/style.css\">
+    <link rel=\"stylesheet\" href=\"/style/login.css\">
+    <title>connection</title>
+</head>
+<body>
+    ";
+        // line 12
+        if ((0 !== twig_compare(($context["error"] ?? null), ""))) {
+            // line 13
+            echo "        <div class=\"error\">";
+            echo twig_escape_filter($this->env, ($context["error"] ?? null), "html", null, true);
+            echo "</div>    
+    ";
+        }
+        // line 15
+        echo "    <header>
+        <nav class=\"topnav\" id=\"myTopnav\">
+          <div id=\"left\">
+            <img class=\"topnav__link img\" src=\"/images/cropped-cropped-NDLP-3-min-e1544695116495.png\">
+            <span class=\"topnav__link\">Notre Dame de la Providence</span>
+          </div>
+          <a class=\"topnav__link\" id=\"right\" href=\"connect.php\">| Login</a>
+        </nav>
+    </header>
+    <main>
+        <div class=\"login_form\">
+                <h1 class=\"login_title\"> Connectez vous </h1>
+                ";
+        // line 28
+        echo "            <form method=\"POST\" >
+                <input class=\"login_inputs\" type=\"email\" name=\"email\" required=\"\" placeholder=\"email\"> <br>
+                <input class=\"login_inputs\" type=\"password\" name=\"password\" required=\"\" placeholder=\"password\"> <br>
+                <input class=\"login_inputs\" id=\"btn5\" type=\"submit\">
+            </form>   
+        </div>
+    </main>
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "connect.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  72 => 28,  58 => 15,  52 => 13,  50 => 12,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "connect.html.twig", "C:\\Users\\luidj\\Documents\\cours bts\\2 année\\projet php poo\\self-feedback1\\templates\\connect.html.twig");
+    }
+}
