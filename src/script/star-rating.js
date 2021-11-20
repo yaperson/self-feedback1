5f30e8d1f6cb7cb83109d8b1f617b8127dbb513c
yanis <yanis.legrand.1@gmail.com>
2021-11-20 17:20:57 +0100

    YLG_galère

--- src/script/star-rating.js
+++ src/script/star-rating.js
@@ -1,10 +1,15 @@
-star = document.getElementsByClassName('rating__star-of')
-radio = document.getElementsByName('rating_repas')
+let star1 = document.getElementById('1')
+let radio = document.getElementsByName('rating_repas')
 
-radio.addEventListener('click', toggleStar, false)
+console.log(star1)
 
-function toggleStar(){
-  if(star.classList === 'rating__star-of') {
-    star.classList.toggle('rating__star-on');
- }
-}
+
+  star1.addEventListener('click', function() {
+    console.log(star1[i])
+
+    if(star1[i].classList === 'rating__star-off') {
+      star1[i].classList.remove('rating__star-off');
+      star1[i].classList.add('rating__star-on');
+
+   }
+  });
