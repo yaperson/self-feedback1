5118d31d550c610b323f557257c8023219957fb0
Un gars <noellor@ndlp.fr>
2021-11-23 15:28:22 +0100

    [BLZ] Je vais cabler.

--- src/script/star-rating.js
+++ src/script/star-rating.js
@@ -2,15 +2,23 @@
 //étoile en sens inverse, voir laquelle est check, activer un flag qui va passer en "on"
 //toutes les étoiles inférieures, et on sera bon. Faudra juste mettre un onhover quand ça marchera.
 
-function init() {
     var starRep = document.getElementsByName("rating_repas");
+    var starEnv = document.getElementsByName("rating_env");
+    var starRepLength = starRep.length-1;
+    var starEnvLength = starEnv.length-1;
+    for(i=0; i<starRep.length; i++) starRep[i].setAttribute("onclick","onRepClick()");
+    for(i=0; i<starEnv.length; i++) starEnv[i].setAttribute("onclick","onEnvClick()");
     var stars = document.getElementsByClassName("rating__star-off");
-}
 function onRepClick() {
+    var i=j=-1;
     
-            for(i=starRep.length; i<0; i--) {
+            for(i=0;i<4;i++) stars[i].setAttribute("class","rating__star-off");
+            for(i=0; i<starRepLength; i++) {
                 if(starRep[i].checked) {
-                    for(j=i; j<0; j--) stars[j].setAttribute("class","rating__star-on");
+                    console.log(i)
+                    for(j=0; j<i; j++) {
+                        stars[j].setAttribute("class","rating__star-on");
+                    }
                     break;
                 }
             }
@@ -18,6 +26,6 @@ function onRepClick() {
 function onEnvClick() {
 
 }
-function itWorks() {
-    window.alert("it works!");
+function aled() {
+    for(i=0; i>stars.length;i++) stars[i].setAttribute("class","rating__star-off");
 }
