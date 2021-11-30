1bc230f0d19f6437d7addce5fc58c5088af5d4b7
Luidjy Aubel <luidjy.aubel@gmail.com>
2021-11-30 18:30:33 +0100

    LAB_Chartjs

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(ChartBuilderInterface $chartBuilder): Response
+    {
+        $date = date('d/m/Y');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([/* ... */]);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'Page d accueil',
+            'date' => $date,
+            'chart' => $chart,
+        ]);
+    }
+}
--- src/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$dsn = 'mysql:dbname=feedback;host=127.0.0.1:3308';
-$usr = 'root';
-$pwd = '';
-
-?> 
