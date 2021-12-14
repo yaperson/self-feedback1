9762066f7a05a7c2fff87a1986f380868317767c
Mars Cahu <dydypot@gmail.com>
2021-12-14 14:51:57 +0100

    form adapt

--- src/Controller/ChartjsController.php
+++ src/Controller/ChartjsController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\UX\Chartjs\Model\Chart;
 use App\Repository\StudentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChartjsController extends AbstractController
 {
