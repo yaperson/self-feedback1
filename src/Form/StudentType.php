45b1896ff080b789abbb70753c7b72a19baaedb2
Mars Cahu <dydypot@gmail.com>
2022-02-22 15:26:54 +0100

    debug username

--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -4,15 +4,13 @@ namespace App\Form;
 
 use App\Entity\Classes;
 use App\Entity\Student;
-use App\Repository\ClassesRepository;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class StudentType extends AbstractType
 {
@@ -21,42 +19,43 @@ class StudentType extends AbstractType
     {
         $builder->add('note_repas', ChoiceType::class, [
             'choices' => [
-                 '1' => 1,
-                 '2' => 2,
-                 '3' => 3,
-                 '4' => 4,
-                 '5' => 5,
-             ],
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+            ],
             'expanded' => true,
             'multiple' => false,
 
-    ]);
-    $builder->add('note_valeur_environnement', ChoiceType::class, [
-        'choices' => [
-             '1' => 1,
-             '2' => 2,
-             '3' => 3,
-             '4' => 4,
-             '5' => 5,
-         ],
-        'expanded' => true,
-        'multiple' => false,
+        ]);
+        $builder->add('note_valeur_environnement', ChoiceType::class, [
+            'choices' => [
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+            ],
+            'expanded' => true,
+            'multiple' => false,
 
-]);
+        ]);
         $builder->add('note_commentaire', TextareaType::class, [
             'attr' => [
                 'rows' => '10',
                 'cols' => '40',
-                'name' => 'note_commentaire'],
+                // 'name' => 'note_commentaire'
+            ],
         ]);
 
         $builder->add('classe', EntityType::class, [
             // looks for choices from this entity
             'class' => Classes::class,
-        
+
             // uses the User.username property as the visible option string
             'choice_label' => 'classe_libelle',
-        
+
             // used to render a select box, check boxes or radios
             // 'multiple' => true,
             // 'expanded' => true,
